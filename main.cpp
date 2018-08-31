c207834811ad91f321aa8b047a212a9dcc04d13e
guitAryan <42151860+guitAryan@users.noreply.github.com>
2018-08-30 21:22:03 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <windows.h>
+#include "Quiz.h"
+#include "AnswerKey.h"
+#include "Checker.h"
+#include "test.h"
+#include "p1Turn.h"
+#include "p2Turn.h"
+using namespace std;
+
+int main()
+{
+    system("color 43");
+    AnswerKey();
+    Quiz();
+
+    return 0;
+}
